671c8168244348a477b22ccd84e9e36706682e3c
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-09-29 20:04:47 -0300

    Added service layer, DTO and GET/games endpoint

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dspesquisa.resources;
+
+import com.devsuperior.dspesquisa.dto.GameDTO;
+import com.devsuperior.dspesquisa.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameResource {
+
+    @Autowired
+    private GameService service;
+
+    @GetMapping
+    public ResponseEntity<List<GameDTO>> findAll() {
+        List<GameDTO> result = service.findAll();
+        return ResponseEntity.ok().body(result);
+    }
+}
