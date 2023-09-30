e0983b8a7233b1e5c7a7a8d264b3ed787aeea77e
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-09-30 14:30:51 -0300

    Add Page home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
