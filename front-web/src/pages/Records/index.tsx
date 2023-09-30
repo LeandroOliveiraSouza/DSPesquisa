d43168a4369a9071adff0aa5e1e0a1cb66db7326
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-09-30 15:07:55 -0300

    Add records route

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,9 +1,32 @@
 import "./styles.css";
 
 const Records = () => {
-    return (
-        <h1>Records</h1>
-    );
-}
+  return (
+    <div className="page-container">
+      <table className="records-table" cellPadding="0" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>INSTANTE</th>
+            <th>NOME</th>
+            <th>IDADE</th>
+            <th>PLATAFORMA</th>
+            <th>GÊNERO</th>
+            <th>TÍTULO DO GAME</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>20/08/2020 13:45</td>
+            <td>João da Silva</td>
+            <td>23</td>
+            <td>Playstation</td>
+            <td>Ação - Aventura</td>
+            <td>The Last of Us 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Records;
